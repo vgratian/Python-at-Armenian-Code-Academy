730d634ca2bbf2ed71b0bc16d70d6f2dbf20ef5f
vgratian <vgratian@utopianlab.am>
2016-05-04 10:18:19 +0400

    first commit simple calc

---
+++ hw02_01/simple_calc.py
@@ -0,0 +1,20 @@
+# Simple Calcultor capable only of one simple calculation
+# Accepted input: integer operator integer (divided by space!)
+# Accepted operators: '+', '-', '*', '/'
+
+string_input = raw_input('Hi, I am Simple Calc 1.0 \nHow can I help you? \n')
+
+# Splits input into 3 elements (integer operator integer)
+split_string = string_input.split(' ')
+
+# Identifies operator and prints result of calculation
+if split_string[1] == '+':
+    print int(split_string[0]) + int(split_string[2])
+elif split_string[1] == '-':
+    print int(split_string[0]) - int(split_string[2])
+elif split_string[1] == '*':
+    print int(split_string[0]) * int(split_string[2])
+elif split_string[1] == '/':
+    print int(split_string[0]) / int(split_string[2])
+else:
+    print 'Error, invalid operator "' + split_string[1] + '" not recognized'
